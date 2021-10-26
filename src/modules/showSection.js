500beb6a36677abc3b6e9616cfc249ea267a8550
Иван Старов <vanyusha.andreev.2022@inbox.ru>
2021-10-26 03:14:20 +0300

    плавная прокрутка к началу страницы при клике на стрелку

--- src/modules/showSection.js
+++ src/modules/showSection.js
@@ -1,23 +1,45 @@
 'use strict';
 
 const moveToSection = () =>{
-    const topMenu = document.querySelector('.top-menu');
+    const topMenu = document.querySelector('.top-menu'),
+        arrowUp = document.querySelector('.up');
 
-    const scrollBlock = (event)=> {
+    if(document.documentElement.clientHeight > 1000){
+        arrowUp.style.display = 'none';
+    }
+
+    const hideArrow = ()=> {
+        window.addEventListener('scroll', ()=> {
+            if(window.scrollY < 550){
+                arrowUp.style.display = 'none';
+            } else if(window.scrollY > 550){
+                arrowUp.style.display = 'block';
+            }
+        }); 
+    };
+    hideArrow();
+
+    const scrollOne = (event)=> {
         event.preventDefault();
 
-        const linkId = event.target.getAttribute('href').substr(1);
-            
-        document.getElementById(linkId).scrollIntoView({
+        let linkId = event.target.getAttribute('href');
+
+        if(event.target.classList.contains('up')){
+            linkId = '.main-wrapper';
+            hideArrow();
+        }
+
+        document.querySelector(linkId).scrollIntoView({
             behavior: 'smooth',
             block:'start'
-        });
-            
-    };    
+        });     
+    };  
+
     topMenu.addEventListener('click', (event)=> {
-        scrollBlock(event);
+        scrollOne(event);
     });
+
+    arrowUp.addEventListener('click', scrollOne);
 };
 
-moveToSection();
 export default moveToSection;
